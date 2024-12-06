1257f59095ab334a718b64b11ee147bbe2baac77
Mark_Jeong <81726531+markjung96@users.noreply.github.com>
2024-12-06 14:48:48 +0900

    fix: prohibit mobile access

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,29 @@
+"use client";
 import { SearchContractWrapper } from "@/src/widgets/Landing/ui/search-contract";
-import Tools from "@/src/widgets/Landing/ui/verification-tools";
 import ProductTitle from "@/src/widgets/Landing/ui/product-title";
+import { useMediaQuery } from "@/src/widgets/Stpper/lib/use-media-query";
 
-export default async function HomeWrapper({ searchParams }: { searchParams?: { [key: string]: string | undefined } }) {
+export default function HomeWrapper({ searchParams }: { searchParams?: { [key: string]: string | undefined } }) {
   const contractAddress = searchParams?.address;
+  const isMobile = useMediaQuery("(max-width: 768px)");
+
   return (
     <div className="h-full flex flex-col items-center justify-center flex-1 px-4 text-center">
-      <ProductTitle />
-      <div className="relative mx-auto mt-12 flex rounded-md focus-within:ring-2 focus-within:ring-blue-500 focus-within:ring-opacity-50">
-        <SearchContractWrapper contractAddress={contractAddress} />
-      </div>
+      {isMobile ? (
+        <div className="font-bold text-lg">
+          Due to the nature of the content, <br />
+          this site does not offer a mobile version. <br />
+          <br />
+          Please access it on a PC.
+        </div>
+      ) : (
+        <>
+          <ProductTitle />
+          <div className="relative mx-auto mt-12 flex rounded-md focus-within:ring-2 focus-within:ring-blue-500 focus-within:ring-opacity-50">
+            <SearchContractWrapper contractAddress={contractAddress} />
+          </div>
+        </>
+      )}
     </div>
   );
 }
